66e81c4128af1c2c001724e23c52cf110c40bd34
Roy Smart <roytsmart@gmail.com>
2023-11-01 21:47:07 -0600

    Added `colorsynth.color_matching_x()` function.

---
+++ colorsynth/_colorsynth.py
@@ -0,0 +1,51 @@
+import numpy as np
+import astropy.units as u
+
+__all__ = [
+    "color_matching_x",
+]
+
+
+def _piecewise_gaussian(
+    x: u.Quantity,
+    mean: u.Quantity,
+    stddev_1: u.Quantity,
+    stddev_2: u.Quantity,
+):
+    where = x < mean
+    not_where = ~where
+    result = np.empty(x.shape)
+    result[where] = np.exp(-np.square((x[where] - mean) / stddev_1) / 2)
+    result[not_where] = np.exp(-np.square((x[not_where] - mean) / stddev_2) / 2)
+    return result
+
+
+def color_matching_x(wavelength: u.Quantity) -> u.Quantity:
+    r"""
+    The CIE 1931 :math:`\overline{x}(\lambda)` color matching function.
+
+    Parameters
+    ----------
+    wavelength
+    """
+    g = _piecewise_gaussian
+    term_1 = 1.056 * g(
+        x=wavelength,
+        mean=599.8 * u.nm,
+        stddev_1=37.9 * u.nm,
+        stddev_2=31.0 * u.nm,
+    )
+    term_2 = 0.362 * g(
+        x=wavelength,
+        mean=442.0 * u.nm,
+        stddev_1=16.0 * u.nm,
+        stddev_2=26.7 * u.nm,
+    )
+    term_3 = -0.065 * g(
+        x=wavelength,
+        mean=501.1 * u.nm,
+        stddev_1=20.4 * u.nm,
+        stddev_2=26.2 * u.nm,
+    )
+    result = term_1 + term_2 + term_3
+    return result
---
+++ colorsynth/_tests/test_colorsynth.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+import astropy.units as u
+import colorsynth
+
+
+@pytest.mark.parametrize(
+    argnames="wavelength",
+    argvalues=[
+        533 * u.nm,
+        np.linspace(
+            start=380 * u.nm,
+            stop=780 * u.nm,
+        )
+    ]
+)
+def test_color_matching_x(
+    wavelength: u.Quantity,
+):
+    result = colorsynth.color_matching_x(wavelength)
+    assert isinstance(result, (float, np.ndarray))
