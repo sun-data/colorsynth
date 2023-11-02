fa2bda9184ce0192ce4b5bd28ded2d4bfb8c6bdc
Roy Smart <roytsmart@gmail.com>
2023-11-01 23:01:08 -0600

    Added `colorsynth.color_matching_y()` function.

--- colorsynth/_colorsynth.py
+++ colorsynth/_colorsynth.py
@@ -3,6 +3,7 @@ import astropy.units as u
 
 __all__ = [
     "color_matching_x",
+    "color_matching_y",
 ]
 
 
@@ -73,3 +74,53 @@ def color_matching_x(wavelength: u.Quantity) -> u.Quantity:
     )
     result = term_1 + term_2 + term_3
     return result
+
+
+def color_matching_y(wavelength: u.Quantity) -> u.Quantity:
+    r"""
+    The CIE 1931 :math:`\overline{y}(\lambda)` color matching function.
+
+    Calculated using the piecewise Gaussian fit method described in
+    :cite:t:`Wyman2013`
+
+    Parameters
+    ----------
+    wavelength
+        the wavelengths at which to evaluate the color-matching function
+
+    Examples
+    --------
+
+    Plot :math:`\overline{y}(\lambda)` over the entire human visible wavelength range
+
+    .. jupyter-execute::
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import astropy.units as u
+        import astropy.visualization
+        import colorsynth
+
+        wavelength = np.linspace(380, 780, num=101) * u.nm
+        ybar = colorsynth.color_matching_y(wavelength)
+
+        with astropy.visualization.quantity_support():
+            plt.figure()
+            plt.plot(wavelength, ybar)
+    """
+    g = _piecewise_gaussian
+    term_1 = 0.821 * g(
+        x=wavelength,
+        mean=568.8 * u.nm,
+        stddev_1=46.9 * u.nm,
+        stddev_2=40.5 * u.nm,
+    )
+    term_2 = 0.286 * g(
+        x=wavelength,
+        mean=530.9 * u.nm,
+        stddev_1=16.3 * u.nm,
+        stddev_2=31.1 * u.nm,
+    )
+    result = term_1 + term_2
+    return result
+
--- colorsynth/_tests/test_colorsynth.py
+++ colorsynth/_tests/test_colorsynth.py
@@ -3,19 +3,26 @@ import numpy as np
 import astropy.units as u
 import colorsynth
 
+wavelengths = [
+    533 * u.nm,
+    np.linspace(
+        start=380 * u.nm,
+        stop=780 * u.nm,
+    ),
+]
 
-@pytest.mark.parametrize(
-    argnames="wavelength",
-    argvalues=[
-        533 * u.nm,
-        np.linspace(
-            start=380 * u.nm,
-            stop=780 * u.nm,
-        ),
-    ],
-)
+
+@pytest.mark.parametrize(argnames="wavelength", argvalues=wavelengths)
 def test_color_matching_x(
     wavelength: u.Quantity,
 ):
     result = colorsynth.color_matching_x(wavelength)
+    assert isinstance(result, (float, np.ndarray))
+
+
+@pytest.mark.parametrize(argnames="wavelength", argvalues=wavelengths)
+def test_color_matching_y(
+    wavelength: u.Quantity,
+):
+    result = colorsynth.color_matching_y(wavelength)
     assert isinstance(result, (float, np.ndarray))
