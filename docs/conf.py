63b63c6878ffbf30407a985383764cab50ae41fe
Roy Smart <roytsmart@gmail.com>
2023-12-07 23:58:21 -0700

    Added Google analytics measurement ID.

--- docs/conf.py
+++ docs/conf.py
@@ -86,6 +86,9 @@ html_theme_options = {
             "icon": "fa-brands fa-python",
         },
     ],
+    "analytics": {
+        "google_analytics_id": "G-4NJ5K5SPLE",
+    },
 }
 
 # https://github.com/readthedocs/readthedocs.org/issues/2569
